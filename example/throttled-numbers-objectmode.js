4cabe8483aa6167e0bba25a460c4df99f301c4d8
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-28 13:06:15 -0400

    adding demo gif

--- example/throttled-numbers-objectmode.js
+++ example/throttled-numbers-objectmode.js
@@ -3,7 +3,7 @@
 var chunkRate = require('../')
   , numbers = require('../test/fixtures/number-readable');
 
-var numbersStream = numbers({ to: 30, throttle: 200 })
+var numbersStream = numbers({ to: 20, throttle: 200 })
 
 var last = 0;
 chunkRate(numbersStream, { objectMode: true })
